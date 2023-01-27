b0294a73b6f15a0cb6ef4bbfc1ae76297f0bf9d2
Shivaji Varma Venkata Pusapati <contact@shivajivarma.com>
2023-01-27 22:45:02 +0530

    generate jwtKey if not present
    Signed-off-by: Shivaji Varma Venkata Pusapati <contact@shivajivarma.com>

--- mod.ts
+++ mod.ts
@@ -1,3 +1,15 @@
 import OS from 'https://raw.githubusercontent.com/justaos/os/v2.1.4/os/mod.ts';
 
+import config from './config.json' assert { type: 'json' };
+import {FileUtils} from "https://raw.githubusercontent.com/justaos/kernel/1.5.1/core/mod.ts";
+
+if (!config.programs.platform.jwtKey) {
+    config.programs.platform.jwtKey = crypto.subtle.generateKey(
+        { name: 'HMAC', hash: 'SHA-512' },
+        true,
+        ['sign', 'verify']
+    )
+    FileUtils.writeJsonFileSync('./config.json', config);
+}
+
 new OS(Deno.cwd()).run();
